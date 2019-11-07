1816c9a1b032a159b5f9addc535b01f0b5d5f07f
gabriel <gbiadeni@gmail.com>
2019-11-06 21:43:39 -0300

    adicionar1

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,11 +2,13 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class Sessao {
 	
 	@Id
