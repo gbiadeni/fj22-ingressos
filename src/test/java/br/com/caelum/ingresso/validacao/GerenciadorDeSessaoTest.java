786eac32aa845a9e15942f5ef9faa5b4461d837a
gabriel <gbiadeni@gmail.com>
2019-11-07 22:14:00 -0300

    ingressos

--- src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
@@ -85,7 +85,7 @@ public class GerenciadorDeSessaoTest {
 	
 	@Test
 	public void oPrecoDaSessaoDeveSerIgualASomaDoPrecoDaSalaMaisOPrecoDoFilme() {
-		Sala sala = new Sala("Eldorado - IMax", new BigDecimal("22.5"));
+		Sala sala = new Sala("Eldorado - IMax", new BigDecimal("22.50"));
 		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12.0"));
 		
 		BigDecimal somaDosPrecosDaSalaEFilme = sala.getPreco().add(filme.getPreco());
